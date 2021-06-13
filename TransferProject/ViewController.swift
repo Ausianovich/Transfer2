c2fd484f1311080a98b9cab58f60c1b88fef66b1
Ausianovich Kanstantsin <smilekostik@yandex.ru>
2021-06-13 16:20:24 +0300

    Initial Commit

---
+++ TransferProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TransferProject
+//
+//  Created by Ausianovich Kanstantsin on 13.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
